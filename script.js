af529364a5679a70f3c907bd5e31608b9c4de49c
Laurent-Ch <chamouleau.laurent@gmail.com>
2021-11-22 09:50:22 +0100

    questions 3 and 4

--- script.js
+++ script.js
@@ -10,22 +10,60 @@ function onFooterClick() {
 
 footer.addEventListener("click", onFooterClick);
 
+//----------------------------------------------------------------------------//
 // Fonctionnalité 2.
 let navbar = document.querySelector("#navbarHeader");
 let navbarTogglerIcon = document.querySelector(".navbar-toggler");
 console.log(navbar);
 console.log(navbarTogglerIcon);
 
-let navbarMenuExpanded = false;
+// let navbarMenuExpanded = false;
 function navbarHamburger() {
-  if (navbarMenuExpanded === false) {
-    navbar.classList.remove("collapse");
-    navbarMenuExpanded = true; 
-  }
-  else {
-    navbar.classList.add("collapse");
-    navbarMenuExpanded = false; 
-  }
+  navbar.classList.toggle("collapse");
+  // if (navbarMenuExpanded === false) {
+  //   navbar.classList.remove("collapse");
+  //   navbarMenuExpanded = true; 
+  // }
+  // else {
+  //   navbar.classList.add("collapse");
+  //   navbarMenuExpanded = false; 
+  // }
 }
 
 navbarTogglerIcon.addEventListener("click", navbarHamburger);
+
+//----------------------------------------------------------------------------//
+// Fonctionnalité 3.
+let firstCard = document.querySelectorAll(".col-md-4")[0];
+
+let firstCardEditBtn = document.querySelectorAll(".col-md-4 .btn-outline-secondary")[0];
+
+function firstEgyptianPlague() {
+  firstCard.style.color = "red";
+}
+
+firstCardEditBtn.addEventListener ("click", firstEgyptianPlague);
+
+//----------------------------------------------------------------------------//
+// Fonctionnalité 4.
+let secondCard = document.querySelectorAll(".col-md-4")[1];
+
+let secondCardEditBtn = document.querySelectorAll(".col-md-4 .btn-outline-secondary")[1];
+
+let ecology = false;
+function captainPlanet() {
+  if (ecology === false) {
+    secondCard.style.color = "green";
+    ecology = true; 
+  }
+  else {
+    secondCard.style.color = "#212529";
+        ecology = false; 
+  }
+}
+
+secondCardEditBtn.addEventListener ("click", captainPlanet);
+
+//----------------------------------------------------------------------------//
+// Fonctionnalité 5.
+
